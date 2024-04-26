5cb86c54a03fb686e8f4c8951f1c7321658a7d77
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-04-26 08:46:54 +0200

    reciclamos el teclado para poder escribir

--- pacman.java
+++ pacman.java
@@ -1,8 +1,13 @@
 class PacMan{
-    private int posX = 0;
-    private int posY = 0;
+    private int posX = 0; /*X es el suelo*/
+    private int posY = 0; /*Y es hacia arriba*/
 
-    public move(){
+    public move(int newPosX, int newPosY){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Cuanto quieres moverte en X: ");
+        int moveX = scanner.nextInt();
+        System.out.print("Cuanto quieres moverte en en Y: ");
+        int moveY = scanner.nextInt();
     }
 }
